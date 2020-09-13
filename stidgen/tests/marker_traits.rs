7f53f9167b7dafef29815c70de39e5a658880ba9
Julien Roncaglia <julien@roncaglia.fr>
2020-09-13 11:17:17 +0200

    Test Sync & Send marker traits
    Fixes #6

---
+++ stidgen/tests/marker_traits.rs
@@ -0,0 +1,16 @@
+use stidgen::string_id;
+
+#[string_id]
+struct FooId(String);
+
+#[test]
+fn test_send() {
+    fn assert_send<T: Send>() {}
+    assert_send::<FooId>();
+}
+
+#[test]
+fn test_sync() {
+    fn assert_sync<T: Sync>() {}
+    assert_sync::<FooId>();
+}
